44a4be958b6c9017487cca1d5f6548439234de71
dhylan01 <dhylan01@users.noreply.github.com>
2022-02-03 23:46:20 -0800

    added tests and java file

---
+++ a.java
@@ -0,0 +1,9 @@
+class a {
+    public static void main(String[] args) {
+        System.out.println(5);
+    }
+
+    public static int mult(int n) {
+        return n * n;
+    }
+}
---
+++ aTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class aTester {
+    @Test
+    public void multiply() {
+        assertEquals(5, a.mult(5));
+    }
+}
